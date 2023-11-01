71b5577c58eeabad97f11b33bd942608479c3ab7
Zsolt Nagy <info@zsoltnagy.eu>
2023-11-02 00:26:35 +0100

    App default content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-    return <div className="App">App.js</div>;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Shopping List</h1>
+            </header>
+
+            <main></main>
+        </div>
+    );
 }
 
 export default App;
