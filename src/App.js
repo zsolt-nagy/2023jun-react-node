c40e374dc766a45e2d7aadcedd68c0ac97530783
Zsolt Nagy <info@zsoltnagy.eu>
2023-11-02 00:38:17 +0100

    Shopping List, Shopping Form and initial load

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import "./App.css";
 
+import React, { useState, useEffect } from "react";
+
+import ShoppingForm from "./Components/ShoppingForm/ShoppingForm";
+import ShoppingList from "./Components/ShoppingList/ShoppingList";
+
 function App() {
+    const [shoppingList, setShoppingList] = useState([]);
+
+    function loadData() {
+        fetch("https://cgrf4m-8080.csb.app/api/list")
+            .then((x) => x.json())
+            .then((response) => setShoppingList(response));
+    }
+
+    useEffect(loadData, []);
+
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Shopping List</h1>
             </header>
 
-            <main></main>
+            <main>
+                <ShoppingForm />
+                <ShoppingList />
+            </main>
         </div>
     );
 }
